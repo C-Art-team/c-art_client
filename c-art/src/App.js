62f3d7feee75c202badd3a0065786c277db2785a
kibuelll <kibuellyoi34@gmail.com>
2022-12-27 15:07:02 +0700

    adding main navbar -- kurang logo + icon

--- c-art/src/App.js
+++ c-art/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import MainNavbar from "./components/MainNavbar/MainNavbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,6 +19,8 @@ function App() {
           Learn React
         </a>
       </header>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1> */}
+      <MainNavbar />
     </div>
   );
 }
