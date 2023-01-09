90a1fa3df12c3566939769aba84a5d91e9ba6f55
agus ali hakim <agusalih@gmail.com>
2023-01-09 10:09:02 +0700

    added table and table row

--- c-art/src/Pages/OrderPage/Order.js
+++ c-art/src/Pages/OrderPage/Order.js
@@ -1,8 +1,31 @@
+import TableRow from "../../components/Tables/TableOrder/TableRow";
+import { useEffect } from "react";
+
 export default function OrderPage() {
+    useEffect(() => {
+
+    })
 
     return (
-        <>
-        order
-        </>
+        <div className="p-10">
+            <div id="titleOrders" className="py-5">
+                My Orders
+            </div>
+            <div className="overflow-x-auto" id="tableOrders">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th>Order Id</th>
+                            <th>Amount</th>
+                            <th>Order Date</th>
+                            <th>Art Ordered</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <TableRow />
+                    </tbody>
+                </table>
+            </div>
+        </div>
     )
 }
---
+++ c-art/src/components/Tables/TableOrder/TableRow.js
@@ -0,0 +1,12 @@
+export default function TableRow() {
+    return (
+        <>
+            <tr>
+                <th>1</th>
+                <td>Cy Ganderton</td>
+                <td>Quality Control Specialist</td>
+                <td>Blue</td>
+            </tr>
+        </>
+    )
+}
