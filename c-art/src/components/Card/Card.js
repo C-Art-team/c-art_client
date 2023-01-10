78306caea5e324ea449e4cae66ae24a775532d4c
satriyo laksono <laksonosatriyo@gmail.com>
2023-01-10 14:38:20 +0700

    add icon music and sound

--- c-art/src/components/Card/Card.js
+++ c-art/src/components/Card/Card.js
@@ -1,9 +1,10 @@
 import React from "react";
 
 import { useNavigate } from "react-router-dom";
+import ImageCategory from "../ImageCategory/ImageCategory";
 import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
 
-function Card({ art, loading, page, id, name, Previews }) {
+function Card({ art, loading, page, id, name, Previews, CategoryId }) {
   // console.log(art.source)
 
   const navigate = useNavigate();
@@ -17,10 +18,10 @@ function Card({ art, loading, page, id, name, Previews }) {
           className="text-gray-700 body-font"
           onClick={() => toNavigation(id)}
         >
-          <div className=" px-10 py-10">
-            <div className="flex flex-wrap -m-4 text-center overflow-hidden">
+          <div className=" px-2 py-2">
+            <div className="flex flex-wrap text-center overflow-hidden">
               <div className=" w-full">
-                <div className="flex flex-col items-center border-2 border-gray-600 px-4 py-10 rounded-lg transition duration-500 hover:scale-110">
+                <div className="flex flex-col h-96 w-full items-center border-2 border-gray-600 px-4 py-10 rounded-lg transition duration-500 hover:scale-110">
                   {loading ? <svg
                     fill="none"
                     stroke="currentColor"
@@ -33,13 +34,13 @@ function Card({ art, loading, page, id, name, Previews }) {
                     <path d="M8 17l4 4 4-4m-4-5v9"></path>
                     <path d="M20.88 18.09A5 5 0 0018 9h-1.26A8 8 0 103 16.29"></path>
                   </svg> :
-                    <img src={Previews[0].sourceUrl} />
+                    <ImageCategory Previews={Previews} CategoryId={CategoryId} />
                   }
                   {loading ? <LoadingSpinner /> : null}
                   <h2 className="title-font font-medium text-3xl text-gray-900">
                     2.7K
                   </h2>
-                  <p className="leading-relaxed">{name}</p>
+                  <p className="leading-relaxed justify-end">{name}</p>
                 </div>
               </div>
             </div>
