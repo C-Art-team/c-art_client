fd61de47f42d3bd35605389698cd1f7015a919b9
kibuelll <kibuellyoi34@gmail.com>
2023-01-11 15:07:27 +0700

    edit profile and development

--- c-art/src/pages/ProfilePage/profilePage.js
+++ c-art/src/pages/ProfilePage/profilePage.js
@@ -9,9 +9,10 @@ import LoadingSpinner from "../../components/LoadingSpinner/LoadingSpinner";
 import { fetchArtByAuthorID } from "../../actions/artAction";
 import { fetchAllOrders } from "../../actions/orderAction";
 import HistoryTableRow from "../../components/Tables/TableHistory/HistoryTableRow";
-
+import Modal from "../../components/Modal/Modal";
 export default function ProfilePage() {
   const [loading, setLoading] = useState(true);
+  const [modal, setModal] = useState(false);
   const profile = useSelector((state) => state.userReducer.oneUser);
   const orders = useSelector((state) => state.orderReducer.orders);
   const lastThreeOrders = orders.slice(0, 3);
@@ -87,6 +88,22 @@ export default function ProfilePage() {
                   alt="Bordered avatar"
                 />
                 <div className="flex flex-col justify-end">
+                  <button onClick={() => setModal(true)}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      class="w-6 h-6"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                      />
+                    </svg>
+                  </button>
                   <h1
                     className="text2xl"
                     name="username"
